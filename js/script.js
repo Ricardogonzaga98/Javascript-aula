28f76c079453c82456a66d2a77d94f958d9dc1b1
Ricardinho <ricardogonzaga98@gmail.com>
2025-03-28 21:33:23 -0300

    javascript

--- js/script.js
+++ js/script.js
@@ -9,13 +9,45 @@
         sum(n1,n2){
             return n1 + n2;
         }
+        subtraction(n1,n2){
+            return n1 - n2;
+        }
+
+        divide(n1,n2){
+            try {
+                if (n2 === 0){
+                    throw new Error('Divisão por zero');
+                }
+                return n1 / n2;
+            } catch (error){
+                return `Erro: ${error.message}`;
+            } 
+        }
+        multiplication(n1,n2){
+            return n1 * n2 ;
+        }
     }
 
     //Instanciar a classe
     const calc = new Calculator();
 
+    // chamando a soma
     let resultado = calc.sum(5,3);
     console.log(resultado);
 
+     // chamando a multiplicação
+     resultado = calc.multiplication(5,6);
+     console.log(resultado);
+     document.querySelector('#result-number'). textContent = resultado;
+
+    // chamando a divisão 
+    resultado = calc.divide(10,0);
+    console.log(resultado); 
+
+    // chamando a subtração 
     resultado = calc.subtraction(10,5);
     console.log(resultado);
+
+  
+
+    
